298cf0346d2b792658a0324bb088c81c155e1eaa
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-05-16 11:59:06 +0200

    Worked on coloring more

--- Colored.py
+++ Colored.py
@@ -1,16 +1,14 @@
 import itertools
 
-from MAXSATSolverColored import MAXSATSolverColored
+from SATSolverColored import SATSolverColored
+from main import timeout
 from mstar import Mstar
 from problem_classes import BaseProblem, MAPFW
+from utils import convert_grid_dict_ints, dijkstra_distance
 
-if __name__ == '__main__':
-    problem = BaseProblem(2, 2, 6, 0.1)
-    print(problem.graph)
-    print(problem.starts)
-    print(problem.goals)
-    res, cost = MAXSATSolverColored(problem).solve()
-    print(res, cost)
+
+@timeout(60.0)
+def mMstar(problem):
     matches = []
     for team in range(len(problem.starts)):
         a = problem.starts[team]
@@ -31,9 +29,37 @@ if __name__ == '__main__':
     for i, problem in enumerate(problems):
         if problem.heuristic > res:
             break
-        path, cost = Mstar(problem.graph, tuple(problem.starts), ((), (), (), ()),
-                        tuple(problem.goals), tsp_cache).solve()
+        path, cost = Mstar(problem.graph, tuple(problem.starts), ((), (), (), (), (), ()),
+                           tuple(problem.goals), tsp_cache).solve()
         if cost < res:
             res = cost
             opt_path = path
-    print(opt_path, res)
+    return opt_path, res
+
+@timeout(60.0)
+def MaxSATColored(problem):
+    return SATSolverColored(problem).solve(True)
+
+@timeout(60.0)
+def SATColored(problem):
+    return SATSolverColored(problem).solve(False)
+
+@timeout(60.0)
+def SATColoredCNF(problem):
+    return SATSolverColored(problem).solve_cnf()
+
+if __name__ == '__main__':
+    problem = BaseProblem(1, 2, 4, 0.1)
+    print(problem.graph)
+    print(problem.starts)
+    print(problem.goals)
+    problem.graph = {0: [4], 2: [6, 3], 3: [2, 7], 4: [0, 8, 5], 5: [4, 9, 6], 6: [2, 5, 10, 7], 7: [3, 6, 11], 8: [4, 12, 9], 9: [5, 8, 13, 10], 10: [6, 9, 14, 11], 11: [7, 10, 15], 12: [8, 13], 13: [9, 12, 14], 14: [10, 13, 15], 15: [11, 14]}
+    problem.starts = [[4, 3]]
+    problem.goals = [[15, 11]]
+    problem.n_agents = 2
+    for vertex in problem.graph:
+        problem.distances[vertex] = dijkstra_distance(problem.graph, vertex)
+    print(MaxSATColored(problem))
+    print(mMstar(problem))
+    print(SATColored(problem))
+    print(SATColoredCNF(problem))
--- SATSolverColored.py
+++ SATSolverColored.py
@@ -1,3 +1,5 @@
+import itertools
+
 from ortools.sat.python import cp_model
 from pysat.card import CardEnc
 from pysat.formula import CNF
@@ -6,30 +8,43 @@ from pysat.solvers import Glucose3
 from MDD import MDD
 
 
-class StandardSolver():
+class SATSolverColored:
 
     def __init__(self, problem):
         self.graph = problem.graph
         self.n_agents = problem.n_agents
         self.starts = problem.starts
-        self.goals = problem.goals
-        self.distances = problem.distances
-        self.heuristic = {}
-        for agent in range(self.n_agents):
-            self.heuristic[agent] = self.distances[self.goals[agent]][self.starts[agent]]
-        self.min_makespan = max(self.heuristic.values())
+        self.options = {}
+        makespans = []
+        self.heuristics = []
+        for team in zip(problem.starts, problem.goals):
+            for start in team[0]:
+                self.options[start] = team[1]
+            opt = float('inf')
+            makespan = float('inf')
+            for matching in [list(zip(perm, team[1])) for perm in itertools.permutations(team[0], len(team[1]))]:
+                heuristic = []
+                for match in matching:
+                    heuristic.append(problem.distances[match[1]][match[0]])
+                if sum(heuristic) < opt:
+                    opt = sum(heuristic)
+                    makespan = max(heuristic)
+            makespans.append(makespan)
+            self.heuristics.append(opt)
+        self.min_makespan = max(makespans)
+        self.starts = [item for sublist in problem.starts for item in sublist]
         self.delta = 0
         self.mdd = {}
         for a in range(self.n_agents):
-            self.mdd[a] = MDD(self.graph, a, self.starts[a], self.goals[a], self.min_makespan)
+            self.mdd[a] = MDD(self.graph, a, self.starts[a], self.options[self.starts[a]], self.min_makespan)
 
-    def solve(self):
+    def solve(self, minimize):
         while True:
             mu = self.min_makespan + self.delta
             for a in range(self.n_agents):
                 if self.delta > 0:
-                    self.mdd[a] = MDD(self.graph, a, self.starts[a], self.goals[a], mu, self.mdd[a])
-            status, solver, path = self.SAT_solver(mu)
+                    self.mdd[a] = MDD(self.graph, a, self.starts[a], self.options[self.starts[a]], mu, self.mdd[a])
+            status, solver, path = self.MAXSAT_solver(mu, minimize)
             if status == 4:
                 break
             self.delta += 1
@@ -37,27 +52,29 @@ class StandardSolver():
         for key in sorted(path.keys(), key=lambda x: (x[0], x[2])):
             if solver.BooleanValue(path[key]):
                 res[key[0]].append(key[1])
-        cost = (mu+1)*self.n_agents
+        cost = (mu + 1) * self.n_agents
         waiting = {i for i in range(self.n_agents)}
+        goals = res[-1]
         for locations in reversed(res):
             for a in range(len(locations)):
-                if a in waiting and locations[a] == self.goals[a]:
+                if a in waiting and locations[a] == goals[a]:
                     cost -= 1
-                if a in waiting and locations[a] != self.goals[a]:
+                if a in waiting and locations[a] != goals[a]:
                     waiting.remove(a)
         return res, cost
 
-    def SAT_solver(self, mu):
-        T = range(mu)
+    def MAXSAT_solver(self, upperbound, minimize):
+        T = range(upperbound)
         vertices = {}
         edges = {}
-        costs = {}
+        time_edges = {}
+        waiting = {}
         model = cp_model.CpModel()
         mdd_vertices = {}
         mdd_edges = {}
         for a in range(self.n_agents):
             mdd_vertices[a] = {}
-            mdd_vertices[a][mu] = {self.goals[a]}
+            mdd_vertices[a][upperbound] = {goal for goal in self.options[self.starts[a]]}
             mdd_edges[a] = {}
             for t in T:
                 mdd_vertices[a][t] = set()
@@ -70,16 +87,23 @@ class StandardSolver():
                     k = nbr[0]
                     mdd_edges[a][t].add((j, k))
                     edges[t, j, k, a] = model.NewBoolVar('edges[%i, %i, %i, %i]' % (t, j, k, a))
-                    if t >= self.heuristic[a] and (j != k or j != self.goals[a]):
-                        costs[t, a, j, k] = model.NewBoolVar('cost[%i, %i, %i, %i]' % (t, a, j, k))
+                    if (t, j, k) not in time_edges:
+                        time_edges[t, j, k] = model.NewBoolVar('time_edges[%i, %i, %i]' % (t, j, k,))
+                    if j == k and (t, j, k) not in waiting:
+                        waiting[t, j, k] = model.NewBoolVar('waiting[%i, %i, %i]' % (t, j, k,))
+        print(len(time_edges))
+        print(len(waiting))
         # Start / End
         for a in range(self.n_agents):
             model.Add(vertices[0, self.starts[a], a] == 1)
-            vertices[mu, self.goals[a], a] = model.NewBoolVar(
-                'vertices[%i, %i, %i]' % (mu, self.goals[a], a))
-            model.Add(vertices[mu, self.goals[a], a] == 1)
+            for goal in self.options[self.starts[a]]:
+                vertices[upperbound, goal, a] = model.NewBoolVar(
+                    'vertices[%i, %i, %i]' % (upperbound, goal, a))
+            model.AddBoolOr([vertices[upperbound, goal, a] for goal in self.options[self.starts[a]]])
         # Constraints
         for a in range(self.n_agents):
+            # No two agents at a vertex at the final timestep
+            model.Add(sum(vertices[upperbound, j, a] for j in mdd_vertices[a][upperbound]) == 1)
             for t in T:
                 # No two agents at a vertex at timestep t
                 model.Add(sum(vertices[t, j, a] for j in mdd_vertices[a][t]) == 1)
@@ -90,6 +114,11 @@ class StandardSolver():
                 for j, k in mdd_edges[a][t]:
                     # 3
                     model.AddBoolAnd(vertices[t, j, a], vertices[t + 1, k, a]).OnlyEnforceIf(edges[t, j, k, a])
+                    # If an agent takes an edge add it to time edges so we can minimize it
+                    if j != k or j not in self.options[self.starts[a]]:
+                        model.AddImplication(edges[t, j, k, a], time_edges[t, j, k])
+                    if j == k and j in self.options[self.starts[a]]:
+                        model.AddImplication(vertices[upperbound, j, a].Not(), time_edges[t, j, k])
                     if j != k:
                         # 4 edited so the edges must be empty
                         model.AddBoolAnd(
@@ -102,13 +131,15 @@ class StandardSolver():
                             # 5
                             if j in mdd_vertices[a2][t]:
                                 model.AddBoolOr(vertices[t, j, a].Not(), vertices[t, j, a2].Not())
-                if t >= self.heuristic[a]:
-                    for j, k in mdd_edges[a][t]:
-                        # 6
-                        if (t, a, j, k) in costs:
-                            model.AddImplication(edges[t, j, k, a], costs[t, a, j, k])
-        # 7
-        model.Add(sum(costs[key] for key in costs) <= self.delta)
+                        if t == upperbound - 1:
+                            for j in mdd_vertices[a][upperbound]:
+                                # 5
+                                if j in mdd_vertices[a2][upperbound]:
+                                    model.AddBoolOr(vertices[upperbound, j, a].Not(), vertices[upperbound, j, a2].Not())
+        if minimize:
+            model.Minimize(sum(time_edges[key] for key in time_edges))
+        else:
+            model.Add(sum(time_edges[key] for key in time_edges) <= sum(self.heuristics) + self.delta)
         solver = cp_model.CpSolver()
         status = solver.Solve(model)
         return status, solver, vertices
@@ -118,7 +149,7 @@ class StandardSolver():
             mu = self.min_makespan + self.delta
             for a in range(self.n_agents):
                 if self.delta > 0:
-                    self.mdd[a] = MDD(self.graph, a, self.starts[a], self.goals[a], mu, self.mdd[a])
+                    self.mdd[a] = MDD(self.graph, a, self.starts[a], self.options[self.starts[a]], mu, self.mdd[a])
             cnf, convert = self.generate_cnf(mu)
             solver = Glucose3()
             solver.append_formula(cnf)
@@ -133,28 +164,29 @@ class StandardSolver():
         res = [[] for _ in range(mu + 1)]
         for key in sorted(path, key=lambda x: (x[0], x[2])):
             res[key[0]].append(key[1])
-        cost = (mu+1)*self.n_agents
+        cost = (mu + 1) * self.n_agents
         waiting = {i for i in range(self.n_agents)}
+        goals = res[-1]
         for locations in reversed(res):
             for a in range(len(locations)):
-                if a in waiting and locations[a] == self.goals[a]:
+                if a in waiting and locations[a] == goals[a]:
                     cost -= 1
-                if a in waiting and locations[a] != self.goals[a]:
+                if a in waiting and locations[a] != goals[a]:
                     waiting.remove(a)
         return res, cost
 
-    def generate_cnf(self, mu):
+    def generate_cnf(self, upperbound):
         cnf = CNF()
         index = 0
-        T = range(mu)
+        T = range(upperbound)
         vertices = {}
         edges = {}
-        costs = {}
+        time_edges = {}
         mdd_vertices = {}
         mdd_edges = {}
         for a in range(self.n_agents):
             mdd_vertices[a] = {}
-            mdd_vertices[a][mu] = {self.goals[a]}
+            mdd_vertices[a][upperbound] = {goal for goal in self.options[self.starts[a]]}
             mdd_edges[a] = {}
             for t in T:
                 mdd_vertices[a][t] = set()
@@ -167,15 +199,18 @@ class StandardSolver():
                     k = nbr[0]
                     mdd_edges[a][t].add((j, k))
                     edges[t, j, k, a] = index = index + 1
-                    if t >= self.heuristic[a] and (j != k or j != self.goals[a]):
-                        costs[t, a, j, k] = index = index + 1
+                    if (t, j, k) not in time_edges:
+                        time_edges[t, j, k] = index = index + 1
         # Start / End
         for a in range(self.n_agents):
             cnf.append([vertices[0, self.starts[a], a]])
-            vertices[mu, self.goals[a], a] = index = index + 1
-            cnf.append([vertices[mu, self.goals[a], a]])
+            for goal in self.options[self.starts[a]]:
+                vertices[upperbound, goal, a] = index = index + 1
+            cnf.append([vertices[upperbound, goal, a] for goal in self.options[self.starts[a]]])
         # Constraints
         for a in range(self.n_agents):
+            # No two agents at a vertex at the final timestep
+            cnf.extend(CardEnc.equals(lits=[vertices[upperbound, key, a] for key in mdd_vertices[a][upperbound]], top_id=cnf.nv, bound=1))
             for t in T:
                 # No two agents at a vertex at timestep t
                 cnf.extend(CardEnc.equals(lits=[vertices[t, key, a] for key in mdd_vertices[a][t]], top_id=cnf.nv, bound=1))
@@ -186,6 +221,9 @@ class StandardSolver():
                     # 3
                     cnf.append([-edges[t, j, k, a], vertices[t, j, a]])
                     cnf.append([-edges[t, j, k, a], vertices[t + 1, k, a]])
+                    # If an agent takes an edge add it to time edges so we can minimize it
+                    if j != k or j not in self.options[self.starts[a]]:
+                        cnf.append([-edges[t, j, k, a], time_edges[t, j, k]])
                     if j != k:
                         # 4 edited so the edges must be empty
                         for a2 in range(self.n_agents):
@@ -197,12 +235,11 @@ class StandardSolver():
                             # 5
                             if j in mdd_vertices[a2][t]:
                                 cnf.append([-vertices[t, j, a], -vertices[t, j, a2]])
-                if t >= self.heuristic[a]:
-                    for j, k in mdd_edges[a][t]:
-                        # 6
-                        if (t, a, j, k) in costs:
-                            cnf.append([costs[t, a, j, k], -edges[t, j, k, a]])
-        # 7
-        cardinality = CardEnc.equals(lits=[costs[key] for key in costs], top_id=cnf.nv, bound=self.delta)
+                        if t == upperbound - 1:
+                            for j in mdd_vertices[a][upperbound]:
+                                # 5
+                                if j in mdd_vertices[a2][upperbound]:
+                                    cnf.append([-vertices[upperbound, j, a], -vertices[upperbound, j, a2]])
+        cardinality = CardEnc.equals(lits=[time_edges[key] for key in time_edges], top_id=cnf.nv, bound=sum(self.heuristics) + self.delta)
         cnf.extend(cardinality.clauses)
         return cnf, {v: k for k, v in vertices.items()}
