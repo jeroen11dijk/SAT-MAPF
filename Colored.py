42047ac6e0dc256dc0e7d3f0c6d20a1df352bca7
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-05-31 12:17:24 +0200

    mstar test once again

--- Colored.py
+++ Colored.py
@@ -7,7 +7,7 @@ from WMStar.mstar import Mstar
 from problem_classes import BaseProblem, MAPFW
 
 
-@timeout(10.0)
+@timeout(300.0)
 def mMstar(problem):
     matches = []
     for team in range(len(problem.starts)):
@@ -41,17 +41,17 @@ def mMstar(problem):
     return opt_path, res
 
 
-@timeout(10.0)
+@timeout(300.0)
 def MaxSATColored(problem):
     return SATSolverColored(problem).solve(True)
 
 
-@timeout(10.0)
+@timeout(300.0)
 def MaxSATColoredInflated(problem):
     return SATSolverColored(problem, inflation=1.25).solve(True)
 
 
-@timeout(10.0)
+@timeout(300.0)
 def SATColoredCNF(problem):
     return SATSolverColored(problem).solve_cnf()
 
@@ -64,33 +64,35 @@ if __name__ == '__main__':
     extra = []
     extra_inflated = []
     graph = 'grid_random_3t_64n_8b_8g_10.0r.graph'
-    for scene in sorted(os.listdir('grid_random_3t_64n_8b_8g_10.0r/'), key=lambda x: int(x.split('_')[7][0:-1])):
-        print(scene)
-        main_problem = BaseProblem(graph, 'grid_random_3t_64n_8b_8g_10.0r/' + scene)
-        ten += 1
-        costs = {"MaxSATColored": -1, "MaxSATColoredInflated": -1, "mMstar": -1, "SATColoredCNF": -1}
-        solvers = [MaxSATColored, MaxSATColoredInflated, mMstar, SATColoredCNF]
-        for func in solvers:
-            if func.__name__ not in done:
-                try:
-                    costs[func.__name__] = func(main_problem)[1]
-                    res[func.__name__] += 1
-                except:
-                    pass
-        if costs["MaxSATColored"] > costs["SATColoredCNF"]:
-            extra.append(((costs["MaxSATColored"] - costs["SATColoredCNF"]) / costs["SATColoredCNF"])*100)
-        if costs["MaxSATColoredInflated"] > costs["SATColoredCNF"]:
-            extra_inflated.append(((costs["MaxSATColoredInflated"] - costs["SATColoredCNF"]) / costs["SATColoredCNF"])*100)
-        if ten == 10:
-            print("\n" + str(scene.split('_')[7][0:-1]) + ": " + str(res))
-            print(extra)
-            print(extra_inflated)
-            file += str(scene.split('_')[7][0:-1]) + ": " + str(res) + '\n'
-            for key in res.keys():
-                if res[key] == 0:
-                    done.add(key)
-            res = {"MaxSATColored": 0, "MaxSATColoredInflated": 0, "mMstar": 0, "SATColoredCNF": 0}
-            ten = 0
-    file += str(extra) + "\n"
-    file += str(extra_inflated)
-    open("results.txt", "w").write(file)
+    # for scene in sorted(os.listdir('grid_random_3t_64n_8b_8g_10.0r/'), key=lambda x: int(x.split('_')[7][0:-1])):
+    #     print(scene)
+    #     main_problem = BaseProblem(graph, 'grid_random_3t_64n_8b_8g_10.0r/' + scene)
+    #     ten += 1
+    #     costs = {"MaxSATColored": -1, "MaxSATColoredInflated": -1, "mMstar": -1, "SATColoredCNF": -1}
+    #     solvers = [MaxSATColored, MaxSATColoredInflated, mMstar, SATColoredCNF]
+    #     for func in solvers:
+    #         if func.__name__ not in done:
+    #             try:
+    #                 costs[func.__name__] = func(main_problem)[1]
+    #                 res[func.__name__] += 1
+    #             except:
+    #                 pass
+    #     if costs["MaxSATColored"] > costs["SATColoredCNF"]:
+    #         extra.append(((costs["MaxSATColored"] - costs["SATColoredCNF"]) / costs["SATColoredCNF"])*100)
+    #     if costs["MaxSATColoredInflated"] > costs["SATColoredCNF"]:
+    #         extra_inflated.append(((costs["MaxSATColoredInflated"] - costs["SATColoredCNF"]) / costs["SATColoredCNF"])*100)
+    #     if ten == 10:
+    #         print("\n" + str(scene.split('_')[7][0:-1]) + ": " + str(res))
+    #         print(extra)
+    #         print(extra_inflated)
+    #         file += str(scene.split('_')[7][0:-1]) + ": " + str(res) + '\n'
+    #         for key in res.keys():
+    #             if res[key] == 0:
+    #                 done.add(key)
+    #         res = {"MaxSATColored": 0, "MaxSATColoredInflated": 0, "mMstar": 0, "SATColoredCNF": 0}
+    #         ten = 0
+    # file += str(extra) + "\n"
+    # file += str(extra_inflated)
+    # open("results.txt", "w").write(file)
+    main_problem = BaseProblem(graph, 'grid_random_3t_64n_8b_8g_10.0r/grid_random_3t_64n_8b_8g_10.0r_6a_0gs_0ss_3types_1.scen')
+    print(mMstar(main_problem))
