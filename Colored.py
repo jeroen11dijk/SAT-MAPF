4956160db20ecd54a5a52d1db5d5f9eedd28ad59
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-05-31 11:20:04 +0200

    Print file to see progress

--- Colored.py
+++ Colored.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     extra_inflated = []
     graph = 'grid_random_3t_64n_8b_8g_10.0r.graph'
     for scene in sorted(os.listdir('grid_random_3t_64n_8b_8g_10.0r/'), key=lambda x: int(x.split('_')[7][0:-1])):
+        print(scene)
         main_problem = BaseProblem(graph, 'grid_random_3t_64n_8b_8g_10.0r/' + scene)
         ten += 1
         costs = {"MaxSATColored": -1, "MaxSATColoredInflated": -1, "mMstar": -1, "SATColoredCNF": -1}
