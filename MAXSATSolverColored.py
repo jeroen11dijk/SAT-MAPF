b7e51fab616833d8ad4ecfdaa800ad73a26995c9
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-06-05 15:17:08 +0200

    remove prints

--- MAXSATSolverColored.py
+++ MAXSATSolverColored.py
@@ -147,7 +147,6 @@ class SATSolverColored:
     def solve_cnf(self, maxsat=False):
         while True:
             mu = self.min_makespan + self.delta
-            print(mu)
             for a in range(self.n_agents):
                 if self.delta > 0:
                     self.mdd[a] = MDD(self.graph, a, self.starts[a], self.options[self.starts[a]], mu, self.mdd[a])
