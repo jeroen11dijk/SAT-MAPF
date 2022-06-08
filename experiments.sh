f6d29370e477e5c1a4c60e75cca9d4b936ddd0c0
jeroen11dijk <jeroen11dijk@gmail.com>
2022-06-08 22:09:03 +0200

    new results

--- experiments.sh
+++ experiments.sh
@@ -6,6 +6,6 @@ do
       now=$(date)
       echo "$now"
       echo grid8.graph grid8/${a}a_${i}.scen
-      timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 1
+      timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 2
     done
 done
