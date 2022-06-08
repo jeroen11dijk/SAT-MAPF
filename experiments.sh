30cd09e1ab102a8d9f64375f841411bbf300ee1f
jeroen11dijk <jeroen11dijk@gmail.com>
2022-06-09 01:30:55 +0200

    results

--- experiments.sh
+++ experiments.sh
@@ -6,9 +6,9 @@ do
       now=$(date)
       echo "$now"
       echo grid8/${a}a_${i}.scen
-      timeout 180 python ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 1
-      timeout 180 python ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 2
-      timeout 180 python ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 3
-      timeout 180 python ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 4
+      timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 1
+      timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 2
+      timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 3
+      timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 4
     done
 done
