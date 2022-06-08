7acbb73871e7f5d170ee600d0f023aeff173d54c
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-06-08 13:03:52 +0200

    this should work

--- experiments.sh
+++ experiments.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-for a in {15..30}
+for a in {0..30}
 do
-  for i in {0..3}
+  for i in {0..9}
     do
-      echo $a
-      echo $i
+      timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 1
     done
 done
