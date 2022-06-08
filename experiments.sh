a5f2b180c256adda6cf00124833d065288c8c24c
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-06-08 15:44:40 +0200

    Add prints to see progress

--- experiments.sh
+++ experiments.sh
@@ -3,6 +3,9 @@ for a in {4..30}
 do
   for i in {0..9}
     do
+      now=$(date)
+      echo "$now"
+      echo grid8.graph grid8/${a}a_${i}.scen
       timeout 180 python3 ColoredMain.py grid8.graph grid8/${a}a_${i}.scen 1
     done
 done
