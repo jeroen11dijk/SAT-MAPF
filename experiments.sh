6f1b18ed5ccfd6e507ac47e7851ff032fb181716
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-06-10 14:23:02 +0200

    32x32 grid added

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for a in {23..100}
+for a in {26..100}
 do
   for i in {0..9}
     do
@@ -13,5 +13,25 @@ do
       now=$(date)
       echo "$now"
       timeout 180 python3 ColoredMain.py grid16.graph grid16/${a}a_${i}.scen 3
+    done
+done
+
+for a in {4..100}
+do
+  for i in {0..9}
+    do
+      echo grid32/${a}a_${i}.scen
+      now=$(date)
+      echo "$now"
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 1
+      now=$(date)
+      echo "$now"
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 2
+      now=$(date)
+      echo "$now"
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 3
+      now=$(date)
+      echo "$now"
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 4
     done
 done
