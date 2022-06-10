39be3f83025402916b6e1dc80f18c3705095c5bf
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-06-10 22:42:07 +0200

    Use python3

--- experiments.sh
+++ experiments.sh
@@ -6,15 +6,15 @@ do
       echo grid32/${a}a_${i}.scen
       now=$(date)
       echo "$now"
-      timeout 180 python ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 1
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 1
       now=$(date)
       echo "$now"
-      timeout 180 python ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 2
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 2
       now=$(date)
       echo "$now"
-      timeout 180 python ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 3
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 3
       now=$(date)
       echo "$now"
-      timeout 180 python ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 4
+      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 4
     done
 done
