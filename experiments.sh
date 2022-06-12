3031ea97681cdce2d0500319226fd75189cd55a1
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-06-12 15:33:21 +0200

    Setup for 1 team

--- experiments.sh
+++ experiments.sh
@@ -3,18 +3,18 @@ for a in {4..100}
 do
   for i in {0..9}
     do
-      echo grid32/${a}a_${i}.scen
+      echo grid8_1/${a}a_${i}.scen
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 1
+      timeout 180 python3 ColoredMain.py grid8_1.graph grid8_1/${a}a_${i}.scen 1
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 2
+      timeout 180 python3 ColoredMain.py grid8_1.graph grid8_1/${a}a_${i}.scen 2
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 3
+      timeout 180 python3 ColoredMain.py grid8_1.graph grid8_1/${a}a_${i}.scen 3
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32.graph grid32/${a}a_${i}.scen 4
+      timeout 180 python3 ColoredMain.py grid8_1.graph grid8_1/${a}a_${i}.scen 4
     done
 done
