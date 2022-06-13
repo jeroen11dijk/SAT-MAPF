1864cb952d5c6a41b581b4d3d3d3712c1ac4416a
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-06-13 15:46:12 +0200

    Carrousel 3 teams

--- experiments.sh
+++ experiments.sh
@@ -3,18 +3,18 @@ for a in {4..100}
 do
   for i in {0..9}
     do
-      echo grid32_1/${a}a_${i}.scen
+      echo carrousel_3/${a}a_${i}.scen
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32_1.graph grid32_1/${a}a_${i}.scen 1
+      timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 1
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32_1.graph grid32_1/${a}a_${i}.scen 2
+      timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 2
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32_1.graph grid32_1/${a}a_${i}.scen 3
+      timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 3
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py grid32_1.graph grid32_1/${a}a_${i}.scen 4
+      timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 4
     done
 done
