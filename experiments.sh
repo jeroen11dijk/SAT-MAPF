a71ecfd19259b0eb76045ec545000d4bd174c003
jeroen11dijk <jeroen11dijk@gmail.com>
2022-06-14 12:59:10 +0200

    Carroussel

--- experiments.sh
+++ experiments.sh
@@ -6,15 +6,9 @@ do
       echo carrousel_3/${a}a_${i}.scen
       now=$(date)
       echo "$now"
-      timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 1
-      now=$(date)
-      echo "$now"
       timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 2
       now=$(date)
       echo "$now"
       timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 3
-      now=$(date)
-      echo "$now"
-      timeout 180 python3 ColoredMain.py carrousel_3.graph carrousel_3/${a}a_${i}.scen 4
     done
 done
