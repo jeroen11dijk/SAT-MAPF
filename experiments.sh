ce242255e3162180cd60640bcd84c962657f31aa
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-07-06 12:24:55 +0200

    Try out 3 minutes again

--- experiments.sh
+++ experiments.sh
@@ -4,11 +4,15 @@ for a in {4..100}
 do
   for i in {0..9}
     do
+      echo file/${a}a_${i}.scen
       now=$(date)
       echo "$now"
-      timeout 300 python3 CombinedMain.py $file.graph $file/${a}a_${i}.scen 2
+      timeout 180 python3 CombinedMain.py $file.graph $file/${a}a_${i}.scen 1
       now=$(date)
       echo "$now"
-      timeout 300 python3 CombinedMain.py $file.graph $file/${a}a_${i}.scen 3
+      timeout 180 python3 CombinedMain.py $file.graph $file/${a}a_${i}.scen 2
+      now=$(date)
+      echo "$now"
+      timeout 180 python3 CombinedMain.py $file.graph $file/${a}a_${i}.scen 3
     done
 done
