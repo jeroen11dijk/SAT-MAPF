544bc1b66947d2e8a3af801a423a3dcf089f7b76
Jeroen van Dijk <jeroen11dijk@gmail.com>
2022-07-12 12:29:13 +0200

    use python 3

--- experiments.sh
+++ experiments.sh
@@ -7,9 +7,9 @@ do
       echo file/${a}a_${i}.scen
       now=$(date)
       echo "$now"
-      timeout 180 python ColoredMain.py $file.graph $file/${a}a_${i}.scen 5
+      timeout 180 python3 ColoredMain.py $file.graph $file/${a}a_${i}.scen 5
       now=$(date)
       echo "$now"
-      timeout 180 python ColoredMain.py $file.graph $file/${a}a_${i}.scen 6
+      timeout 180 python3 ColoredMain.py $file.graph $file/${a}a_${i}.scen 6
     done
 done
